b90961b506aad413697ed99c082ea2cf02a40d49
Mateus Junior <juniormateusknd@gmail.com>
2023-08-07 11:56:07 -0300

    Enable unicode support using CHCP command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -612,6 +612,9 @@ Set-PSReadLineKeyHandler -Key End `
     }
 }
 
+# Enable unicode support
+chcp 936
+
 refreshenv | out-null
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/json.omp.json" | Invoke-Expression | out-null
 Clear-Host
