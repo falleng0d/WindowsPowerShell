b988917bcf2fa5b4a0dd44290eff8f945df7e2cc
Mateus Junior <mateus@matj.dev>
2025-01-28 13:29:35 +0100

    feat: Add `..` alias to go back

--- Modules/AliasDefinitions.psm1
+++ Modules/AliasDefinitions.psm1
@@ -3,7 +3,6 @@ Import-Module -Name $PSScriptRoot\Utils.psm1 -DisableNameChecking
 #Aliases
 Set-Alias -Name ep -Value Edit-Profile | out-null
 Set-Alias -Name tch -Value Test-ConsoleHost | out-null
-Set-Alias -Name tch -Value Test-ConsoleHost | out-null
 Set-Alias -Name gfl -Value Get-ForwardLink | out-null
 Set-Alias -Name gwp -Value Get-WebPage | out-null
 Set-Alias -Name rifc -Value Replace-InvalidFileCharacters | out-null
@@ -21,4 +20,8 @@ Set-Alias -Name scratch -Value New-Scratch
 Set-Alias -Name touch -Value New-File
 Set-Alias -Name rpp -Value Reload-Profile
 
+function Set-ParentLocation { Set-Location .. }
+Set-Alias -Name '..' -Value Set-ParentLocation
+
+Export-ModuleMember -Function Set-ParentLocation
 Export-ModuleMember -Alias *
