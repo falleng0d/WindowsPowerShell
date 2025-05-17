1db0b3ec5325ee44a07c531be863891ed54166f5
Mateus Junior <mateus@matj.dev>
2025-05-17 11:46:39 +0200

    chore(Tasks): Update paths in Start-RemoteControl scripts for consistency

--- Tasks/Start-RemoteControl.bat
+++ Tasks/Start-RemoteControl.bat
@@ -4,7 +4,7 @@ set _Process=remotecontrol.exe
 
 tasklist /FI "IMAGENAME eq %_Process%" /V | findstr /C:%_Process% > nul
 if errorlevel 1 (
-    cd C:\Users\falleng0d\Dropbox\Projects\remotecontrol\build\windows\x64\runner\Release
+    cd "C:\Program Files\remotecontrol"
     explorer remotecontrol.exe
     cd /d %~dp0
     powershell -NoLogo -NonInteractive -NoProfile -File Minimize-RemoteControl.ps1
