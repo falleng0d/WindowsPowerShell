be1c1d96120b4ee6df547c395ed5353fddc8b4bc
Mateus Junior <mateus@matj.dev>
2025-11-21 13:33:36 +0100

    feat(Utils): Add export function to set environment variables using bash-style syntax

--- Modules/Utils.psm1
+++ Modules/Utils.psm1
@@ -149,6 +149,50 @@ function wget {
     Invoke-WebRequest -Uri $URL -OutFile $FileName
 }
 
+function export {
+    <#
+    .SYNOPSIS
+    Mimics the bash export command to set environment variables.
+
+    .DESCRIPTION
+    Sets environment variables in PowerShell using bash-style syntax.
+    Supports formats like: export VAR=VALUE or export VAR="VALUE"
+
+    .PARAMETER Expression
+    The variable assignment expression (e.g., "VAR=VALUE" or "VAR='VALUE'")
+
+    .EXAMPLE
+    export PATH=/usr/local/bin:$PATH
+    export NODE_ENV=production
+    export API_KEY="secret123"
+    #>
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$Expression
+    )
+
+    # Parse the expression to extract variable name and value
+    if ($Expression -match '^([a-zA-Z_][a-zA-Z0-9_]*)=(.*)$') {
+        $varName = $matches[1]
+        $varValue = $matches[2]
+
+        # Remove surrounding quotes if present
+        if ($varValue -match '^[''"](.*)[''""]$') {
+            $varValue = $matches[1]
+        }
+
+        # Set the environment variable
+        [System.Environment]::SetEnvironmentVariable($varName, $varValue, [System.EnvironmentVariableTarget]::Process)
+
+        # Also set it in the current session's env: drive for immediate access
+        Set-Item -Path "env:$varName" -Value $varValue
+
+        Write-Verbose "Set environment variable: $varName = $varValue"
+    } else {
+        Write-Error "Invalid export syntax. Use: export VAR=VALUE"
+    }
+}
+
 function Update-NpmDependencies {
     param (
         [Parameter(Mandatory=$false)]
@@ -236,3 +280,4 @@ Export-ModuleMember -Function Get-VsCodeExtension
 Export-ModuleMember -Function Get-RedirectedUrl
 Export-ModuleMember -Function wget
 Export-ModuleMember -Function Update-NpmDependencies
+Export-ModuleMember -Function export
