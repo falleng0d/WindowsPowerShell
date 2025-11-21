099b62671ae8fd83e6383146186e110f8ed0ccd5
Mateus Junior <mateus@matj.dev>
2025-11-21 13:33:28 +0100

    fix(AutoComplete): Refactor PSReadLine options and key handlers for improved functionality

--- Modules/AutoComplete.psm1
+++ Modules/AutoComplete.psm1
@@ -18,15 +18,15 @@ Function Test-CommandExists {
     }
 }
 
+# Works only if the console output supports virtual terminal processing and is not redirected
 try {
     Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 } catch {
     return
 }
-
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 try {
     Set-PsFzfOption -TabExpansion
@@ -41,7 +41,7 @@ try {
 # Set-PSReadLineKeyHandler -Key Ctrl+Backspace -Function ShellBackwardKillWord
 Set-PSReadLineKeyHandler -Key Ctrl+h -Function ShellBackwardKillWord
 Set-PSReadLineKeyHandler -Key Alt+Backspace -Function BackwardKillWord
-Set-PSReadLineKeyHandler -key Enter -Function ValidateAndAcceptLine
+Set-PSReadLineKeyHandler -key Enter -Function AcceptLine
 #Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
 #Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
 #Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
