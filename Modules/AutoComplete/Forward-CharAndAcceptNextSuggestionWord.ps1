15efb37b188111918e3460e95fe825bc4bacbaac
Mateus Junior <mateus@matj.dev>
2026-03-23 17:18:00 +0100

    refactor(Modules): Restructure AutoComplete module into subdirectory with separated key handlers and completion scripts

---
+++ Modules/AutoComplete/Forward-CharAndAcceptNextSuggestionWord.ps1
@@ -0,0 +1,14 @@
+function Invoke-ForwardCharAndAcceptNextSuggestionWordKeyHandler {
+    param($key, $arg)
+
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    if ($cursor -lt $line.Length) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
+    }
+    else {
+        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
+    }
+}
---
+++ Modules/AutoComplete/Show-HistoryKeyHandler.ps1
@@ -0,0 +1,41 @@
+function Show-HistoryKeyHandler {
+    $pattern = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
+    if ($pattern) {
+        $pattern = [regex]::Escape($pattern)
+    }
+
+    $history = [System.Collections.ArrayList]@(
+        $last = ''
+        $lines = ''
+        foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath)) {
+            if ($line.EndsWith('`')) {
+                $line = $line.Substring(0, $line.Length - 1)
+                $lines = if ($lines) {
+                    "$lines`n$line"
+                }
+                else {
+                    $line
+                }
+                continue
+            }
+
+            if ($lines) {
+                $line = "$lines`n$line"
+                $lines = ''
+            }
+
+            if (($line -cne $last) -and (!$pattern -or ($line -match $pattern))) {
+                $last = $line
+                $line
+            }
+        }
+    )
+    $history.Reverse()
+
+    $command = $history | Out-GridView -Title History -PassThru
+    if ($command) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
+    }
+}
