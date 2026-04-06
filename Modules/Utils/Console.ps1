e90aeb01c45020ecc2390902ed3c211afd86db06
Mateus Junior <mateus@matj.dev>
2026-04-06 17:10:21 +0200

    feat(Console): Add Is-InteractiveAndVisible function to check console interactivity

--- Modules/Utils/Console.ps1
+++ Modules/Utils/Console.ps1
@@ -9,3 +9,33 @@ function Enter-Admin {
 
     Start-Process -Verb RunAs (Get-Process -Id $PID).Path
 }
+
+function Is-InteractiveAndVisible {
+    # language=cs
+    Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+
+public class Utils
+{
+    [DllImport("kernel32.dll")]
+    private static extern uint GetFileType(IntPtr hFile);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetStdHandle(int nStdHandle);
+
+    public static bool IsInteractiveAndVisible
+    {
+        get
+        {
+            return Environment.UserInteractive &&
+                GetFileType(GetStdHandle(-10)) == 2 &&   // STD_INPUT_HANDLE is FILE_TYPE_CHAR
+                GetFileType(GetStdHandle(-11)) == 2 &&   // STD_OUTPUT_HANDLE
+                GetFileType(GetStdHandle(-12)) == 2;     // STD_ERROR_HANDLE
+        }
+    }
+}
+'@
+
+    return [Utils]::IsInteractiveAndVisible
+}
